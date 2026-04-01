cd1d82e32b59d62ca236dcee33fca26f1b771059
Manmohan Sharma <manmohan.dev122@gmail.com>
2026-04-01 19:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let openBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close-modal");
+let modal = document.querySelector(".modal");
+
+openBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", ()=>{
+	modal.style.display = "none";
+})
+window.addEventListener("click", (e)=>{
+	if(e.target == modal){
+	modal.style.display = "none";
+	}
+})
